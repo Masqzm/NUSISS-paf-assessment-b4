ad421efaed8de466116ed9ab0f430c53b19518d5
Hazim <mhazima95@gmail.com>
2025-02-10 16:12:58 +0800

    Complete up till Task 2.3

---
+++ data/task2.js
@@ -0,0 +1,45 @@
+db.listings_and_reviews.findOne()
+
+db.listings_and_reviews.aggregate([
+    {
+        $match: {
+            'address.country': {
+                $regex: 'Australia',
+                $options: 'i'
+            }
+        }
+    },
+    {
+        $out: 'listings'    // copy over aggregation to new collection
+    }
+])
+
+//db.listings_and_reviews.drop()
+
+db.listings.findOne()
+db.listings.find({}).limit(2)
+
+db.listings.aggregate([
+    {
+        $unwind: '$reviews'
+    },
+    {
+        $project: { 
+            _id: '$reviews._id',
+            date: '$reviews.date',
+            listing_id: '$reviews.listing_id',
+            reviewer_name: { $replaceAll: { input: '$reviews.reviewer_name', find: ',', replacement: '' } },
+            comments: { 
+                $replaceAll: { 
+                    input: { $replaceAll: { input: '$reviews.comments', find: '\n', replacement: '' } }, 
+                    find: '\r', 
+                    replacement: '' }
+            }
+        }
+    },
+    {
+        $out: 'reviews'    // copy over aggregation to new collection
+    }
+])
+
+db.reviews.findOne()
